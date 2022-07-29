f3383af5fd95107dc0bef25730453630c797c23b
Sean Critz <sarcose@gmail.com>
2022-07-29 00:20:20 -0500

    first commit

---
+++ conf.lua
@@ -0,0 +1,10 @@
+
+function love.conf(t)
+	t.identity	=	"write"
+	t.window.resizable = true
+	t.window.fullscreen = false
+	t.console = true
+	t.window.width = 1000
+	t.window.height = 600
+--	t.window.fullscreentype = "desktop"
+end
