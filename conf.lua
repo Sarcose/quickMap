ea97d16ec248c58d509b6bfb6900b8c463d1be00
Sean Critz <sarcose@gmail.com>
2022-07-29 16:17:33 -0500

    turn off console

--- conf.lua
+++ conf.lua
@@ -3,7 +3,7 @@ function love.conf(t)
 	t.identity	=	"write"
 	t.window.resizable = true
 	t.window.fullscreen = false
-	t.console = true
+	t.console = false
 	t.window.width = 1000
 	t.window.height = 600
 --	t.window.fullscreentype = "desktop"
